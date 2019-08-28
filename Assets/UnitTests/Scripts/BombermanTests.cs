72fb8f66f72a0aee1119e814d171a12af90c81b8
Chumplumps <47583539+Chumplumps@users.noreply.github.com>
2019-08-28 15:57:29 +1000

    Don't really know

---
+++ Assets/UnitTests/Scripts/BombermanTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.TestTools;
+using NUnit.Framework;
+public class BombermanTests
+{
+    private GameObject game;
+    private Player[] players;
+
+    Player GetPlayer(int index)
+    {
+        foreach (var player in players)
+        {
+            if(player.playerNumber == index)
+            {
+                return player;
+            }
+        }
+        return null;
+    }
+
+    [SetUp]
+    public void SetUp()
+    {
+        GameObject gamePrefab = Resources.Load<GameObject>("Prefabs/Game");
+        game = Object.Instantiate(gamePrefab);
+        players = Object.FindObjectsOfType<Player>();
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerDropsBomb()
+    {
+        Player player1 = GetPlayer(1);
+
+        player1.DropBomb();
+
+        yield return new WaitForEndOfFrame();
+
+        Bomb bomb = Object.FindObjectOfType<Bomb>();
+
+        Assert.IsTrue(bomb != null, "The Bomb didn't spawn");
+
+        Debug.Break();
+    }
+
+    [UnityTest]
+    public IEnumerator PlayerMovement()
+    {
+        Player player1 = GetPlayer(1);
+        player1.Up
+    }
+
+    [TearDown]
+    void TearDown()
+    {
+        Object.Destroy(game);
+    }
+}
